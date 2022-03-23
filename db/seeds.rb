89433a02e7c9aa91a5c102fa5290eb26f9197235
Valentine Fouche <fouche.valentine@gmail.com>
2022-03-23 13:14:22 +0100

    ajouté les routes et les controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Socca', address: 'rue Bonaparte', phone_number: '0499668877', category: 'french')
+
+Restaurant.create(name: 'Pomodoro', address: 'rue Panama', phone_number: '0787663874', category: 'italian')
+
+Restaurant.create(name: 'Makoto', address: 'rue Cangrejo', phone_number: '0839325877', category: 'japanese')
+
+Restaurant.create(name: 'La bonne frite', address: 'rue Unefois', phone_number: '0378118877', category: 'belgian')
+
+Restaurant.create(name: 'Le pangolin', address: 'rue Wuhan', phone_number: '0197389277', category: 'chinese')
